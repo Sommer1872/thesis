7630861b5a56d9096a358b9714464a8ee0ac98f0
Simon <simonwsommer@gmail.com>
2019-09-24 11:00:58 +0200

    unzip in parallel

--- data_handling/unzip_files.py
+++ data_handling/unzip_files.py
@@ -3,45 +3,47 @@
 """
 import os
 import gzip
+from multiprocessing import Pool
 from pathlib import Path
 import shutil
+from typing import List
 
 from tqdm import tqdm
 
 
 def main():
 
-    home = os.path.expanduser("~")
-    zipped_path = Path.home() / "data/ITCH_market_data/zipped"
-    binary_path = Path.home() / "data/ITCH_market_data/binary"
-
-    unzip_files(zipped_path, binary_path)
+    zipped_dir = Path.home() / "data/ITCH_market_data/zipped"
+    zipped_filepaths = sorted([path for path in zipped_dir.glob("*.gz")])
+    results = unzip_all(zipped_filepaths)
 
 
-def unzip_files(zipped_directory: str, new_directory: str):
+def unzip_one_file(zipped_file_path: Path):
 
-    os.makedirs(new_directory, exist_ok=True)
+    unzipped_dir = zipped_file_path.parents[1] / "unzipped"
+    os.makedirs(unzipped_dir, exist_ok=True)
 
-    zipped_filepaths = sorted([path for path in zipped_directory.glob("*.gz")])
+    new_filename = zipped_file_path.stem
+    new_filepath = unzipped_dir / new_filename
 
-    for this_filepath in tqdm(zipped_filepaths):
+    if Path.exists(new_filepath):
+        return f"Path {new_filepath} already exists, skipped"
 
-        new_filename = this_filepath.stem
-        new_filepath = new_directory / new_filename
+    # unzip it and save new file
+    with gzip.open(zipped_file_path, "rb") as f_in:
+        with open(new_filepath, "wb") as f_out:
+            shutil.copyfileobj(f_in, f_out)
 
-        if Path.exists(new_filepath):
-            tqdm.write(f"Path {new_filepath} already exists, skipped")
-            continue
+    # delete the .gz file
+    os.remove(zipped_file_path)
 
-        # unzip it and save new file
-        with gzip.open(this_filepath, "rb") as f_in:
-            with open(new_filepath, "wb") as f_out:
-                shutil.copyfileobj(f_in, f_out)
+    return f"File unzipped and saved to {new_filepath}"
 
-        tqdm.write(f"File unzipped and saved to {new_filepath}")
 
-        # delete the .gz file
-        # os.remove(this_filepath)
+def unzip_all(file_paths: List[Path]) -> List[str]:
+    with Pool(processes=os.cpu_count() - 1) as pool:
+        results = list(tqdm(pool.imap_unordered(unzip_one_file, file_paths), total=len(file_paths)))
+        return results
 
 
 if __name__ == "__main__":
