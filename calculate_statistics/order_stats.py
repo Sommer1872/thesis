c253004ac90eadabbd415a5bffa59284b6e75eee
Simon <simonwsommer@gmail.com>
2019-10-18 10:47:47 +0200

    updated order stats calculation

--- calculate_statistics/order_stats.py
+++ calculate_statistics/order_stats.py
@@ -53,25 +53,32 @@ def calculate_order_stats(
     order_stats["distance_in_ticks"] = round(
         order_stats["distance_to_best"] / order_stats["tick_size"], 2
     )
+    order_stats[["price", "best_price", "distance_to_best", "tick_size"]] /= (
+        10 ** metainfo.price_decimals
+    )
+
     # only look at orders that have been entered at most 1 tick away from best
     columns = ["price", "quantity_entered", "quantity_filled", "distance_in_ticks"]
     close_to_best = order_stats.loc[order_stats.distance_in_ticks <= 1, columns]
-    close_to_best[["price", "best_price", "distance_to_best", "tick_size"]] /= (
-        10 ** metainfo.price_decimals
-    )
-    value_entered = np.sum(close_to_best["price"] * close_to_best["quantity_entered"])
-    value_filled = np.sum(close_to_best["price"] * close_to_best["quantity_filled"])
-    if value_entered > 0:
-        fill_ratio = value_filled / value_entered
-    else:
-        fill_ratio = np.nan
+
+    close_to_best["value_entered"] = close_to_best["price"] * close_to_best["quantity_entered"]
+    close_to_best["value_filled"] = close_to_best["price"] * close_to_best["quantity_filled"]
 
     stats = dict()
     stats["average_time_to_fill"] = time_to_fill.get("mean", np.nan)
     stats["median_time_to_fill"] = time_to_fill.get("50%", np.nan)
-    stats["value_entered"] = value_entered
-    stats["value_filled"] = value_filled
-    stats["fill_ratio"] = fill_ratio
+
+    stats["mean_value_entered"] = np.mean(close_to_best["value_entered"])
+    stats["median_value_entered"] = np.median(close_to_best["value_entered"])
+    stats["total_value_entered"] = np.sum(close_to_best["value_entered"])
+
+    stats["mean_value_filled"] = np.mean(close_to_best["value_filled"])
+    stats["median_value_filled"] = np.median(close_to_best["value_filled"])
+    stats["total_value_filled"] = np.sum(close_to_best["value_filled"])
+
+    stats["mean_fill_ratio"] = np.mean(close_to_best["value_filled"] / close_to_best["value_entered"])
+    stats["median_fill_ratio"] = np.median(close_to_best["value_filled"] / close_to_best["value_entered"])
+    stats["total_fill_ratio"] = np.sum(close_to_best["value_filled"]) / np.sum(close_to_best["value_entered"])
 
     return stats
 
