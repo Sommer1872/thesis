5bb926304ed27ed27e0c807c143e3aa359c0d608
Simon <simonwsommer@gmail.com>
2019-10-19 08:56:01 +0200

    process daily statistics in parallel, too

--- process_all.py
+++ process_all.py
@@ -7,7 +7,7 @@ from multiprocessing import Pool
 import os
 from pathlib import Path
 import pickle
-from typing import List, Iterator
+from typing import List, Iterator, Dict
 
 # third-party packages
 import pandas as pd
@@ -20,7 +20,7 @@ from daily_statistics.process import process_daily_statistics
 
 def main():
 
-    start_time = pd.Timestamp("now").strftime("%Y%m%d_%H-%M-%S")
+    start_time = pd.Timestamp("now").strftime("%Y.%m.%d %H:%M:%S")
     print(f"Started at {start_time}")
 
     data_path = Path.home() / "data/ITCH_market_data/unzipped"
@@ -42,7 +42,7 @@ def main():
         pickle.dump(daily_stats, output_file, protocol=pickle.HIGHEST_PROTOCOL)
 
     # further processing
-    results = process_daily_statistics(daily_stats)
+    results = combine_daily_statistics(daily_stats)
 
     timestamp = pd.Timestamp("now").strftime("%Y%m%d_%H-%M-%S")
     filepath = stats_path / f"{timestamp}_daily_stats.csv"
@@ -52,21 +52,39 @@ def main():
     print(f"\n {5*'    '} <<<<< Done >>>>> \n")
 
 
-def load_and_process_all(file_paths: Iterator[Path]) -> List[tuple]:
+def load_and_process_all(file_paths: Iterator[Path]) -> List[Dict]:
     with Pool(processes=os.cpu_count() - 1) as pool:
-        results = list()
+        daily_stats = list()
         parallel_processes = pool.imap_unordered(
             load_and_process_orderbook_stats, file_paths
         )
-        for result in tqdm(parallel_processes):
-            results.append(result)
-    return results
+        for single_day_statistics_output in tqdm(parallel_processes):
+            daily_stats.append(single_day_statistics_output)
+    return daily_stats
 
 
 def load_and_process_orderbook_stats(file_path: Path):
     this_day_imi_data = SingleDayIMIData(file_path)
     this_day_imi_data.process_messages()
-    results = calculate_orderbook_stats(this_day_imi_data)
+    single_day_stats = calculate_orderbook_stats(this_day_imi_data)
+    return single_day_stats
+
+
+def combine_daily_statistics(daily_stats: List[Dict]):
+    with Pool(processes=os.cpu_count() - 1) as pool:
+        all_results = list()
+        parallel_processes = pool.imap_unordered(
+            process_daily_statistics, daily_stats
+        )
+        for daily_result in tqdm(parallel_processes):
+            all_results.append(daily_result)
+
+    # combine everything into one big dataframe
+    results = pd.concat(all_results)
+    del all_results
+    results.reset_index(inplace=True)
+    results.set_index("isin", inplace=True)
+    results.rename(columns={"index": "orderbook_no"}, inplace=True)
     return results
 
 
