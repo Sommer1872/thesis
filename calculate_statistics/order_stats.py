cb81ccc12932f0a8512e6b9f62f59ff3af5df572
Simon <simonwsommer@gmail.com>
2019-11-24 10:08:45 +0100

    Update order_stats.py

--- calculate_statistics/order_stats.py
+++ calculate_statistics/order_stats.py
@@ -16,10 +16,12 @@ def calculate_order_stats(
     start_microsecond: int,
     end_microsecond: int,
 ) -> Dict[str, float]:
+
     if order_stats.empty:
         return empty_result()
     order_stats.set_index("entry_time", inplace=True)
     order_stats.sort_index(inplace=True)
+    
     # filter for start/end
     order_stats = order_stats.loc[start_microsecond:end_microsecond]
     # remove market orders
@@ -73,15 +75,6 @@ def calculate_order_stats(
 
     stats = dict()
 
-    stats["num_orders_total"] = order_stats.shape[0]
-    counts = order_stats["first_fill_time"].isna().value_counts()
-    for nan_indicator in counts.index:
-        num_orders = counts[nan_indicator]
-        if nan_indicator is True:
-            stats["num_orders_deleted"] = num_orders
-        else:
-            stats["num_orders_filled"] = num_orders
-
     # only look at orders that have been entered at most 1 tick away from best
     columns = [
         "price",
@@ -93,13 +86,6 @@ def calculate_order_stats(
     ]
     close_to_best = order_stats.loc[order_stats.distance_in_ticks <= 1, columns]
 
-    close_to_best["value_entered"] = (
-        close_to_best["price"] * close_to_best["quantity_entered"]
-    )
-    close_to_best["value_filled"] = (
-        close_to_best["price"] * close_to_best["quantity_filled"]
-    )
-
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", category=RuntimeWarning)
         
@@ -107,25 +93,6 @@ def calculate_order_stats(
         stats["time_to_fill_median"] = close_to_best["time_to_fill"].median()
         stats["time_to_removal_mean"] = close_to_best["time_to_removal"].mean()
         stats["time_to_removal_median"] = close_to_best["time_to_removal"].median()
-
-        stats["value_entered_mean"] = close_to_best["value_entered"].mean()
-        stats["value_entered_median"] = close_to_best["value_entered"].median()
-        stats["value_entered_total"] = close_to_best["value_entered"].sum()
-
-        stats["value_filled_total"] = close_to_best["value_filled"].sum()
-
-        stats["fill_ratio_mean"] = (
-            close_to_best["value_filled"] / close_to_best["value_entered"]
-        ).mean()
-        stats["fill_ratio_median"] = (
-            close_to_best["value_filled"] / close_to_best["value_entered"]
-        ).median()
-        if stats["value_entered_total"] > 0:
-            stats["fill_ratio_total"] = (
-                stats["value_filled_total"] / stats["value_entered_total"]
-            )
-        else:
-            stats["fill_ratio_total"] = 0
 
     return stats
 
